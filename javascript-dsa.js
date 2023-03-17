270ede74b176c772e9a6519152d8c11641a9cba5
mdrahmatullahamn <rmd831219@gmail.com>
2023-03-17 12:49:29 +0530

    up date

--- javascript-dsa.js
+++ javascript-dsa.js
@@ -1,27 +1,44 @@
-var array=[34,45,65,23,5,676,32,34]
-var target =66
-var flag=false
-for(var i=0;i<array.length;i++){
-    if(array[i]===target)
-    flag=true
-}if(flag===true){
-    console.log("found")
-}else{
-    console.log("not found")
-};
+Q . 1
+// var array=[34,45,65,23,5,676,32,34]
+// var target =66
+// var flag=false
+// for(var i=0;i<array.length;i++){
+//     if(array[i]===target)
+//     flag=true
+// }if(flag===true){
+//     console.log("found")
+// }else{
+//     console.log("not found")
+// };
+Q .3
+// var number = 19;
+// var array = [3,5,79,45,0,-67,23,77,19,78,12]
 
-var number = 19;
-var array = [3,5,79,45,0,-67,23,77,19,78,12]
+// var flag= false;
+// for(var i=0;i<array.length;i++){
+//     if(array[i]===number)
+//     flag = true
+// }
+// if(flag===true){
+//     console.log("index")
+// }else{
+//     console.log("Not found")
+// }
+    Q . 4
+var array =[34,45,567,78,89,34,67,87,34,78,89];
+var index={}
+ var flag = false;
+ for(var i=0;i<array.length;i++){
+     if(array[i]===index){
+        flag=true;
+     }
+     index[i]       
 
-var flag= false;
-for(var i=0;i<array.length;i++){
-    if(array[i]===number)
-    flag = true
-}
-if(flag===true){
-    console.log("index")
-}else{
-    console.log("Not found")
-}
+     
+ }
+ Q . 3
+ var start=5768;
+ var end = 78656
+ console.log(++end,start)
     
 
