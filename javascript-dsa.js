6a3933c63d9a18a02ec00ba3c9d27ab60bf8e456
mdrahmatullahamn <rmd831219@gmail.com>
2023-03-17 11:58:25 +0530

    update

---
+++ javascript-dsa.js
@@ -0,0 +1,11 @@
+var array=[34,45,65,23,5,676,32,34]
+var target =66
+var flag=false
+for(var i=0;i<array.length;i++){
+    if(array[i].target)
+    flag=true
+}if(flag===true){
+    
+}else{
+    console.log("not found")
+}
