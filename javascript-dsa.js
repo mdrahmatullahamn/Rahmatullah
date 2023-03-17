2fc16f156775c8aa9a29d384aa2ace9360a13b74
mdrahmatullahamn <rmd831219@gmail.com>
2023-03-17 12:15:59 +0530

    update

--- javascript-dsa.js
+++ javascript-dsa.js
@@ -2,10 +2,26 @@ var array=[34,45,65,23,5,676,32,34]
 var target =66
 var flag=false
 for(var i=0;i<array.length;i++){
-    if(array[i].target)
+    if(array[i]===target)
     flag=true
 }if(flag===true){
-    
+    console.log("found")
 }else{
     console.log("not found")
+};
+
+var number = 19;
+var array = [3,5,79,45,0,-67,23,77,19,78,12]
+
+var flag= false;
+for(var i=0;i<array.length;i++){
+    if(array[i]===number)
+    flag = true
 }
+if(flag===true){
+    console.log("index")
+}else{
+    console.log("Not found")
+}
+    
+
